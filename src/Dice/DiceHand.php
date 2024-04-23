5f3b1d1ce023f842aabf6a64837413d621ee59b6
Mikael Roos <mikael.t.h.roos@gmail.com>
2024-04-23 15:43:57 +0200

    Add files from kmom04 tuesday zoom

--- src/Dice/DiceHand.php
+++ src/Dice/DiceHand.php
@@ -34,6 +34,15 @@ class DiceHand
         return $values;
     }
 
+    public function sum(): int
+    {
+        $sum = 0;
+        foreach ($this->hand as $die) {
+            $sum += $die->getValue();
+        }
+        return $sum;
+    }
+
     public function getString(): array
     {
         $values = [];
